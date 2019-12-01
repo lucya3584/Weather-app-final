a8f949b399c57fe02347f520b4926f69c9ca5794
lucya3584 <lucya3584@yahoo.co.uk>
2019-12-01 17:47:25 +0000

    complete up to icons

---
+++ src/components/forecast-summaries.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ForecastSummary from './forecast-summary';
+import '../styles/app.scss';
+import '../styles/forecast-summaries.scss';
+
+const ForecastSummaries = props => (
+  <div className="forecast-summaries">
+    {props.forecasts.map(forecast => (
+      <ForecastSummary
+        date={forecast.date}
+        description={forecast.description}
+        icon={forecast.icon}
+        temperature={forecast.temperature.max}
+      />
+    ))}
+  </div>
+);
+
+export default ForecastSummaries;
