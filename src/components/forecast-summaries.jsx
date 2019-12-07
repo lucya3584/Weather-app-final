ee50e36d314d2af7432b234bb059a2eaaa5ad937
lucya3584 <lucya3584@yahoo.co.uk>
2019-12-07 12:40:55 +0000

    latest updates

--- src/components/forecast-summaries.jsx
+++ src/components/forecast-summaries.jsx
@@ -4,16 +4,22 @@ import '../styles/app.scss';
 import '../styles/forecast-summaries.scss';
 
 const ForecastSummaries = props => (
+
   <div className="forecast-summaries">
-    {props.forecasts.map(forecast => (
-      <ForecastSummary
-        date={forecast.date}
-        description={forecast.description}
-        icon={forecast.icon}
-        temperature={forecast.temperature.max}
-      />
-    ))}
+    {
+      props.forecasts.map(forecast => (
+        <ForecastSummary
+          key={forecast.date}
+          date={forecast.date}
+          description={forecast.description}
+          icon={forecast.icon}
+          temperature={forecast.temperature.max}
+        />
+      ))
+    }
   </div>
 );
+
+
 
 export default ForecastSummaries;
--- src/components/forecast-summary.jsx
+++ src/components/forecast-summary.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import WeatherIcon from 'react-icons-weather';
+import moment from 'moment';
 
 const ForecastSummary = props => (
-  <div className="forecast-summary">
-    <span className="date">{props.date}</span>
-    <span className="temperature">{props.temperature}</span>
-    <span className="description">{props.description}</span>
-    <span className="icon">{props.icon}</span>
+  <div>
+    <div className="forecast-summary_icon">
+      <WeatherIcon name="owm" iconId={props.icon} />
+    </div>
+
+    <div>
+      <span className="date">{props.date}</span>
+      <span className="temperature">{props.temperature}</span>
+      <span className="description">{props.description}</span>
+      <span className="icon">{props.icon}</span>
+    </div>
+
+    <div className="forecast-summary_date">
+      <span>{moment(props.date).format('ddd Do MMM')}</span>
+    </div>
   </div>
 );
 
