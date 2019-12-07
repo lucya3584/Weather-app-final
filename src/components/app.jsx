fd69d73f1a55ec68d745cc041a6b5159c1eff8c8
lucya3584 <lucya3584@yahoo.co.uk>
2019-12-07 15:50:38 +0000

    completed forecast-details, formatted as class

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,35 +3,58 @@ import PropTypes from 'prop-types';
 import LocationDetails from './location-details';
 import ForecastSummary from './forecast-summary';
 import ForecastSummaries from './forecast-summaries';
+import ForecastDetails from './forecast-details';
 import '../styles/app.scss';
 import '../styles/forecast-summaries.scss';
 
-const App = props => (
-  <div className="forecast">
-    <LocationDetails city={props.location.city} country={props.location.country} />
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-    {/* <ForecastSummary
-      date={props.forecasts.date}
-      temperature={props.forecasts.temperature}
-      description={props.forecasts.description}
-      icon={props.forecasts.icon}
-    /> */}
+    this.state = {
+      selectedDate: this.props.forecasts[0].date,
+    };
+  }
 
-    <ForecastSummaries forecasts={props.forecasts} />
-  </div>
-);
+  render() {
+    const selectedForecast = this.props.forecasts.find(
+      forecast => forecast.date === this.state.selectedDate,
+    );
 
-App.propTypes = {
-  location: PropTypes.shape({
-    city: PropTypes.string,
-    country: PropTypes.string,
-  }).isRequired,
-  forecasts: PropTypes.array.isRequired,
-};
+    return (
+      <div>
+        <LocationDetails city={this.props.location.city} country={this.props.location.country} />
 
-//   date: PropTypes.string.isRequired,
-//   temperate: PropTypes.number.isRequired,
-//   description: PropTypes.string.isRequired,
-// }).isRequired,
+        <ForecastSummaries forecasts={this.props.forecasts} />
+
+        <ForecastDetails forecast={selectedForecast} />
+      </div>
+    );
+  }
+}
+
+// const App = props => (
+//   <div className="forecast">
+//     <LocationDetails city={props.location.city} country={props.location.country} />
+
+//     {/* <ForecastSummary
+//       date={props.forecasts.date}
+//       temperature={props.forecasts.temperature}
+//       description={props.forecasts.description}
+//       icon={props.forecasts.icon}
+//     /> */}
+
+//     <ForecastSummaries forecasts={props.forecasts} />
+//     <ForecastDetails forecast={props.forecasts[0]} />
+//   </div>
+// );
+
+// App.propTypes = {
+//   location: PropTypes.shape({
+//     city: PropTypes.string,
+//     country: PropTypes.string,
+//   }).isRequired,
+//   forecasts: PropTypes.array.isRequired,
+// };
 
 export default App;
