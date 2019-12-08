1af3bed8d644c9607256083ddf43ac571f06ef2b
lucya3584 <lucya3584@yahoo.co.uk>
2019-12-08 12:21:01 +0000

    button not linked yet

--- src/components/forecast-summaries.jsx
+++ src/components/forecast-summaries.jsx
@@ -3,22 +3,24 @@ import ForecastSummary from './forecast-summary';
 import '../styles/app.scss';
 import '../styles/forecast-summaries.scss';
 
-const ForecastSummaries = props => (
+const ForecastSummaries = props => {
+  return (
 
-  <div className="forecast-summaries">
-    {
-      props.forecasts.map(forecast => (
-        <ForecastSummary
-          key={forecast.date}
-          date={forecast.date}
-          description={forecast.description}
-          icon={forecast.icon}
-          temperature={forecast.temperature.max}
-        />
-      ))
-    }
-  </div>
-);
+    <div className="forecast-summaries">
+      {
+        props.forecasts.map(forecast => (
+          <ForecastSummary
+            key={forecast.date}
+            date={forecast.date}
+            description={forecast.description}
+            icon={forecast.icon}
+            temperature={forecast.temperature.max}
+          />
+        ))
+      }
+    </div>
+  )
+}
 
 
 
