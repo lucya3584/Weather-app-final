b7b4474763945f134fcd01ec9b2960b3817d6087
lucya3584 <lucya3584@yahoo.co.uk>
2019-12-08 12:21:35 +0000

    button not linked, temp not added

--- src/components/forecast-summary.jsx
+++ src/components/forecast-summary.jsx
@@ -3,28 +3,35 @@ import PropTypes from 'prop-types';
 import WeatherIcon from 'react-icons-weather';
 import moment from 'moment';
 import '../styles/forecast-summaries.scss';
+import '../styles/forecast-summary.scss';
 
 class ForecastSummary extends React.Component {
   render() {
     return (
-
       <div className="forecast-summary">
-        <span className='date'>{moment(this.props.date).format('ddd Do MMM')}</span>
-        <span className='maxTemp'>Max temp: {this.props.temperature.max}</span>
-        <span className='desc'>{this.props.description}</span>
-        <span className='icon'><WeatherIcon name="owm" iconId={this.props.icon} flip="horizontal" rotate="90" /></span>
-
-
+        <span className="date">{moment(this.props.date).format('ddd Do MMM')}</span>
+        <span className="maxTemp">
+          Max temp:
+          {this.props.temperature.max}
+        </span>
+        <span className="desc">{this.props.description}</span>
+        <span className="icon">
+          <WeatherIcon name="owm" iconId={this.props.icon} flip="horizontal" rotate="90" />
+        </span>
+        <span className="details">
+          
+<button onClick={() => this.props.onSelect(this.props.date)}>More details</button>
+        </span>
       </div>
-    )
+    );
   }
-
-};
+}
 ForecastSummary.propTypes = {
   forecasts: PropTypes.shape({
     date: PropTypes.number.isRequired,
     temperate: PropTypes.number.isRequired,
     description: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired,
   }).isRequired,
 };
 
