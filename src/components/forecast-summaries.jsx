051ff4852b0b56a8b64caf4b8a8053977fb73382
lucya3584 <lucya3584@yahoo.co.uk>
2019-12-08 18:17:41 +0000

    more styling and buttons linked

--- src/components/forecast-summaries.jsx
+++ src/components/forecast-summaries.jsx
@@ -3,25 +3,22 @@ import ForecastSummary from './forecast-summary';
 import '../styles/app.scss';
 import '../styles/forecast-summaries.scss';
 
-const ForecastSummaries = props => {
-  return (
-
-    <div className="forecast-summaries">
-      {
-        props.forecasts.map(forecast => (
+class ForecastSummaries extends React.Component {
+  render() {
+    return (
+      <div className="forecast-summaries">
+        {this.props.forecasts.map(forecast => (
           <ForecastSummary
             key={forecast.date}
             date={forecast.date}
             description={forecast.description}
             icon={forecast.icon}
             temperature={forecast.temperature.max}
+            onSelect={this.props.onForecastSelect}
           />
-        ))
-      }
-    </div>
-  )
+        ))}
+      </div>
+    );
+  }
 }
-
-
-
 export default ForecastSummaries;
